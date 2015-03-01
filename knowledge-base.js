61911864361fa864d48881ff1fa5fac18a60bca2
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2015-02-28 21:22:05 -0300

    knowledge base

--- knowledge-base.js
+++ knowledge-base.js
@@ -1,3 +1,4 @@
+/* globals Component, async */
 'use strict';
 
 var KnowledgeBase;
@@ -11,13 +12,49 @@ KnowledgeBase.provide('IKnowledgeBase', function (done) {
 });
 
 KnowledgeBase.install(function (done) {
-  this.ask = function () {}.bind(this);
+  this.animals = ['abelha', 'animal robo', 'aranha viuva negra', 'aranha', 'atum', 'baleia', 'beija-flor', 'besouro',
+    'boi', 'borboleta', 'braquiossauro', 'cabra', 'cachorro', 'camarao', 'canarinho', 'canguru', 'capivara', 'caramujo',
+    'cavalo', 'coala', 'cobra', 'diabo-da-tasmania', 'dodo', 'equidna', 'escorpiao', 'esquilo', 'estegossauro', 'foca',
+    'formiga', 'furao', 'gaivota', 'galo', 'gato', 'gaviao', 'girafa', 'golfinho', 'humano', 'jacare', 'lacraia',
+    'leao', 'lesma', 'libelula', 'lobo', 'louva-deus', 'macaco', 'mastodonte', 'mergulhao', 'mico-leao-dourado',
+    'minhoca', 'morcego', 'morsa', 'mosca', 'mosquito', 'ornitorrinco', 'ovelha', 'paquicefalossauro', 'pato',
+    'percevejo', 'pikachu', 'pinguim', 'piranha', 'planaria', 'poliqueta', 'pomba', 'porco', 'pterodactil', 'raposa',
+    'rato', 'regaleco', 'rinoceronte', 'salamandra', 'sapo', 'tamandua', 'tartaruga', 'tatu', 'tigre-da-tasmania',
+    'tigre', 'tiranossauro', 'trilobita', 'tubarao', 'urso', 'zebra', 'zumbi'];
 
-  done();
+  this.questions = {};
+
+  async.each(this.animals, function (animal, next) {
+    var xhr;
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        this.questions[animal] = xhr.responseText.split('\n').map(function (question) {
+          return question.split(',');
+        }).filter(function (data) {
+          return data.length === 2;
+        }).map(function (question) {
+          return {
+            'question' : question[0].replace(/\"/g, ''),
+            'answer'   : question[1].lastIndexOf('sim') > -1
+          };
+        });
+        next();
+      }
+    }.bind(this);
+    xhr.open('GET', './bd/' + animal + '.txt', true);
+    xhr.send();
+  }.bind(this), done);
 });
 
 KnowledgeBase.install(function (done) {
-  this.retrieve = function () {}.bind(this);
+  this.list = function () {
+    return this.animals;
+  }.bind(this);
+
+  this.retrieve = function (animal) {
+    return this.questions[animal];
+  }.bind(this);
 
   done();
 });
